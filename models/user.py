a04744493e2eeb0503f1fad759c8891b23965d25
enzonjagi <amosndungo@gmail.com>
2022-01-24 10:33:35 +0300

    User model debug: typo

--- models/user.py
+++ models/user.py
@@ -16,4 +16,3 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-    places = relationship("Place")
