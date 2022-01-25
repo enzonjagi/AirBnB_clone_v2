61254ad332e5a09f1fde3caca0fbda7cf7fa8ea1
enzonjagi <amosndungo@gmail.com>
2022-01-25 16:38:17 +0300

    Restart nginx and add a symlink

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,29 +1,23 @@
 #!/usr/bin/env bash
 # Setup configurations for a customised nginx setup
-DIR=/data
-DIR2=/data/web_static
-DIR3=/data/web_static/releases/test
-DIR4=/data/web_static/shared
-# check if folders exist and set them up
-if [ ! -d "$DIR" ] && [ ! -d "$DIR2" ] && [ ! -d "$DIR3" ] && [ ! -d "$DIR4" ];
-then
-    sudo mkdir /data/
-    sudo mkdir /data/web_static/
-    sudo mkdir /data/web_static/releases/
-    sudo mkdir /data/web_static/shared/
-    sudo mkdir /data/web_static/releases/test/
-fi
+sudo apt-get update
+sudo apt-get install -y nginx
+# mkdir /data/
+sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+
 # change ownership of data folder to ubuntu ser and group
 sudo chown ubuntu:ubuntu /data/*
 # simple html file for configuration purposes
-echo "nginx set!!" >> /data/web_static/releases/test/index.html
+echo "<h1>nginx set!!</h2>" >> /data/web_static/releases/test/index.html
 # check if symbolink link exists and delete it then install
 SYMLINK=/data/web_static/current
 if [ -f "$SYMLINK" ]; then
-    sudo unlink /data/web_static/current
-fi
+    sudo rm -rf /data/web_static/current;
+fi;
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-
+# change ownership of data folde\r to ubuntu ser and group
+sudo chown -hR ubuntu:ubuntu /data/
 # Update nginx config to set the above into motion
-sudo sed -i "/listen 80 default_server/a \\\tlocation /hbnb_static { alias /
-data/web_static/current; }"  /etc/nginx/sites-available/default
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+sudo service nginx restart
