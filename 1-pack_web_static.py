5e2f3a58b898b5045f4a40cefb9c42c146ea77f1
enzonjagi <amosndungo@gmail.com>
2022-01-26 06:47:20 +0300

    pack into .tgz

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,25 +2,27 @@
 '''Places items from the web_static
 folder into a tgz archive
 '''
-from fabric.api import *
+from fabric.api import local
+from datetime import datetime
+from fabric.decorators import runs_once
 
 
-env.hosts = ['localhost']
-
-
+@runs_once
 def do_pack():
     '''packs web_static into a .tgz file'''
     # add files from web_static to tgz archive
     # all archives are stored in /versions in the archive
-    run("mkdir versions/")
+    local("mkdir -p versions")
     # name of archive to follow format
     # web_static_<year><month><day><hour><minute><secon\d>.tgz
-    timestamp = dt.now().strftime("%Y_%m_%d_%H_%M_%S")
-    name = "web_static_" + timestamp + ".tgz"
-    result = run("tar -cvzf versions/%s.tgz /web_static", name)
+    timestamp = datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
+    name = ("versions/web_static_{}.tgz".
+            format(timestamp))
+    result = local("tar -cvzf {} /web_static".
+                   format(name), capture=True)
     # returns correctly generated archive on success
     if result.succeeded:
-        return (result)
+        return (name)
     # else none
     else:
         return(None)
