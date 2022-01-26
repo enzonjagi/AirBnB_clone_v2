e83f5fa9a77351909978ea9750c5bcce64bd973a
enzonjagi <amosndungo@gmail.com>
2022-01-26 07:49:33 +0300

    Deploy the static to servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,35 +15,34 @@ env.hosts = ['35.231.58.85', '34.138.244.202']
 env.user = "ubuntu"
 
 
-@runs_once
 def do_deploy(archive_path):
     '''Does the magic of the script'''
     if not exists(archive_path):
         return(False)
     # upload archive to /tmp/ directory
     try:
-        file_name = archive_path.split("/")[-1].split(".")[0]
+        file_ne = archive_path.split("/")[-1].split(".")[0]
         put(archive_path, "/tmp/")
 
-        run("mkdir -p /data/web_static/releases/{}".format(file_name))
+        sudo("mkdir -p /data/web_static/releases/{}".format(file_ne))
 
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".
-            format(file_name, file_name))
+        sudo("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".
+             format(file_ne, file_ne))
 
-        run('rm -rf /tmp/{}.tgz'.format(file_name))
+        sudo('rm -rf /tmp/{}.tgz'.format(file_ne))
 
-        run(('mv /data/web_static/releases/{}/web_static/* ' +
+        sudo(('mv /data/web_static/releases/{}/web_static/* ' +
              '/data/web_static/releases/{}/').
-            format(file_name, file_name))
+             format(file_ne, file_ne))
 
-        run('rm -rf /data/web_static/releases/{}/web_static'.
-            format(file_name))
+        sudo('rm -rf /data/web_static/releases/{}/web_static'.
+             format(file_ne))
 
-        run('rm -rf /data/web_static/current')
+        sudo('rm -rf /data/web_static/current')
 
-        run(('ln -s /data/web_static/releases/{}/' +
+        sudo(('ln -s /data/web_static/releases/{}/' +
              ' /data/web_static/current').
-            format(file_name))
+             format(file_ne))
 
         return(True)
     except Exception:
