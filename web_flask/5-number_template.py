481c338c79a7248582a0b341a5922a463f4b7024
enzonjagi <amosndungo@gmail.com>
2022-02-03 08:08:39 +0300

    Routing to a teomplate if input is valid

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -47,8 +47,7 @@ def number_route(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template_route(n):
     '''Sorts the /number_template/<n> route'''
-    value = "Number: {}".format(n)
-    return render_template('5-number.html', value=value)
+    return render_template('5-number.html', value=n)
 
 
 if __name__ == '__main__':
