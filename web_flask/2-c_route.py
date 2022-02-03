e7d4efbfc637367620597fd1b9f3fe2473515901
enzonjagi <amosndungo@gmail.com>
2022-02-03 07:23:17 +0300

    Return a text provided in the route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''Creates a /,  /hbnb, and /c/<text >routes'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    '''Sorts the / route'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    '''Sorts the /hbnb route'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    '''Sorts the /c/<text> route'''
+    text = text.replace("_", " ")
+    return text
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
